789d7ba9d484f250ee321b94010040efef2c8ad0
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-08 15:32:29 -0700

    files added

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,77 @@
 const express = require('express');
-
+const Posts = require('./postDb');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+// handle every request that begins with /api/posts
+router.get('/', (req, res) => { 
+  Posts.get(req.query)
+    .then( posts => {
+      res.status(200).json({ posts });
+    })
+    .catch( err => {
+      console.log( err );
+      res.status(500).json({
+        message: "Error retrieving the posts"
+      });
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  Posts.getById(req.params.id)
+    .then( post => {
+      if ( post ) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Post not found." });
+      }
+    })
+    .catch( err => {
+      console.log( err );
+      res.status(500).json({
+        message: 'Error retrieving the specific post.'
+      });
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+
+  Posts.remove(id)
+    .then( count => {
+      if ( count ) {
+        res.status(200).json({ message: "Post deleted." });
+      } else {
+        res.status(404).json({ message: "Post not found" });
+      }
+    })
+    .catch( err => {
+      console.log( err );
+      res.status(500).json({ error: "500: Internal error occurred." })
+    })
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const changes = req.body
+
+  Posts.update(req.params.id, changes)
+    .then( count => {
+      if ( count ) {
+        Posts.getById(req.params.id)
+          .then( post => {
+            res.status(200).json(post)
+          })
+          .catch( err => {
+            res
+              .status(500)
+              .json({ errorMessage: "Error reading the updated post."});
+          });
+      } else {
+        res.status(404).json({ message: "The post could not be found." })
+      }
+    })
+    .catch( err => {
+      res.sttatus(500).json({ message: "Error updating the post." })
+    })
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
 
+// middleware
+// server.use(logger);
+server.use(helmet());
+server.use(morgan('short'));
+server.use(express.json());
+
+// endpoints
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2 style='text-align: center; margin-top: 20px;'>Let's write some middleware!: 4000</h2>`);
 });
 
-//custom middleware
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
 
-function logger(req, res, next) {}
+//function logger(req, res, next) {}
 
 module.exports = server;
